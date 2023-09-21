7c047ea40ff5d2e7f06cdf5d4c9f0be4b791e67e
Ashwin Raja <ashwinraja941@tamu.edu>
2023-09-21 10:05:14 -0500

    admin page sprint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+name = "Fish Pond"
+address = "Military Walk, College Station, TX 77840"
+Location.create(name:, address:)
+
+name = "Evans library"
+address = "400 Spence St, College Station, TX 77843"
+Location.create(name:, address:)
