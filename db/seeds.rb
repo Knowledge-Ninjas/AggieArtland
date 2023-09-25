328a971aca641c44698cca5c4c285540858c0758
Ashwin Raja <ashwinraja941@tamu.edu>
2023-09-25 18:04:09 -0500

    Added functionality to add art pieces to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ description = "The 31st Street Mural Project is located on the north side of the
 ArtPiece.create(name:, address:, description:)
 
 name = "A&M United Methodist Church Annex"
-address = "A&M United Methodist Church Annex, 3315 Tauber St, College Station, TX 77840"
+address = " 3315 Tauber St, College Station, TX 77840"
 artist = "Kaitlyn McCollum '22"
 description = "Visit this newest addition to the Howdy murals around town in the Northgate Entertainment District, next to the A&M United Methodist Church Annex. It features defining aspects of both Texas A&M and the church, painted by an Aggie and a member of the congregation as a welcome to Aggieland and their church."
-ArtPiece.create(name:, address:, description:)
+ArtPiece.create(name:, address:, description:, artist:)
