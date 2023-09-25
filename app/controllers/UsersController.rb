0245be100016fc168757aab086cf4b8bfbd6721b
Srijith <srijithka72@gmail.com>
2023-09-25 17:28:01 -0500

    login and session controllers added

--- app/controllers/UsersController.rb
+++ app/controllers/UsersController.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  include ApplicationHelper
+  before_action :require_login, only: [:show]
+
     def new
       @user = User.new
     end
@@ -6,6 +9,7 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
+        session[:user_id] = @user.id
         redirect_to @user, notice: 'User successfully created!'
       else
         render 'new'
@@ -20,5 +24,12 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
+
+    def require_login
+      unless logged_in?
+        flash[:error] = 'You must be logged in to access this section.'
+        redirect_to login_path
+      end
     end
   end
