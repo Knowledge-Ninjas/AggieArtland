ac837daa302060cbb6fa3bbf482a4c832fae0b3e
18PatZ <18patz@students.harker.org>
2023-11-10 18:07:48 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -33,17 +33,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_213904) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.float "latitude"
-    t.float "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["latitude"], name: "index_locations_on_latitude"
-    t.index ["longitude"], name: "index_locations_on_longitude"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
